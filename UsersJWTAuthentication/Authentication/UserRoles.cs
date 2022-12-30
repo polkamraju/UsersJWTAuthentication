84723d109af8b954988f45e3a85f4f90a4e33ec9
rpolkam <rpolkam@RPOLKAM-LAP>
2022-12-30 18:42:35 +0530

    Add project files.

---
+++ UsersJWTAuthentication/Authentication/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersJWTAuthentication.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string SuperUser = "SuperUser";
+        public const string User = "User";
+    }
+}
